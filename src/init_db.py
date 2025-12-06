5c1393e8ffedefe6bb28e2c8dc5aa748d167fb85
Wakura A <baglogtnmalang@gmail.com>
2025-12-06 18:32:00 +0700

    Tambah kembali init_db.py untuk inisialisasi database

--- src/init_db.py
+++ src/init_db.py
@@ -1,7 +1,4 @@
-# D:\G\Aplikasikoe\src\init_db.py
 import sys, os
-
-# Tambahkan folder root project ke sys.path
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from app_toko import toko_app, db
